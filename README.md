50c0ef907a35ff333958c2143d3ee9dce12725de
Paul Sayre <psayre23+github@gmail.com>
2012-03-10 13:22:40 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,114 @@
+WebSQL is a simple wrapper for the now deprecated WebSQL Storage API from the W3C (http://www.w3.org/TR/webdatabase/).
+This is particularly useful for building large webapps which store and process data locally. I built this abstraction out
+of frustration with the API. A common method for quering a database is this:
+
+	var db = openDatabase('mydb', '1.0', 'My Database', 5 * 1024 * 1024);
+	db.transaction(function (tx) {
+		tx.executeSql('SELECT * FROM products WHERE category_id = ?', [catID], function (tx, res) {
+
+			var products = [];
+			for(var i = 0; i < res.rows.length; i++) {
+				products[i] = res.rows.item(i);
+			}
+			// Do something with product
+			displayProducts(products);
+
+		}, function (tx, err) {
+			throw new Error(err.message);
+		});
+	});
+
+This coding style is really painful. Moreover, if you have to use the results of one query for another, and one error's out
+you are left with a _lot_ of bean counting. I _hate_ bean counting. And, to make matters worse, if you are inserting a
+bunch of content you had to repeat the insert string over and over again.
+
+WebSQL, But Better
+------------------
+
+Since the transaction API is still useful, you can make a request for a transaction in a similar way:
+
+	var db = WebSQL('mydb');
+	db.rawTx(function (tx) {
+		tx.executeSql('SELECT * FROM products WHERE category_id = ?', [catID], function (tx, res) {
+
+			var products = [];
+			for(var i = 0; i < res.rows.length; i++) {
+				products[i] = res.rows.item(i);
+			}
+			// Do something with product
+			displayProducts(products);
+
+		}, function (tx, err) {
+			throw new Error(err.message);
+		});
+	});
+
+There are a few use cases that I care about. The first is executing a bunch of SQL, in order, and getting back results.
+This is what the WebSQL.query function is for:
+
+	var db = WebSQL('mydb');
+	db.query('SELECT * FROM products WHERE category_id = ?', [catID])
+		.fail(function (error) {
+			throw new Error(error.message);
+		})
+		.done(function (products) {
+			displayProducts(products);
+		});
+
+See how much simplier that is? This abstraction uses the Deferred/Promise system that a lot of non-blocking code is moving
+towards on the web. I intend to support jQuery's Deferred object with a custom module $.WebSQL() in the future which will
+use the $.Deferred() object instead of my own.
+
+Second, I want to be able to insert a whole bunch of data, but without having to do separate transactions. Query can take
+several SQL statements at once, and will return the last result set. On top of that, it will process each item in an array
+data as if it was the last query. Here is all that combined:
+
+	var db = WebSQL('mydb');
+	db.query(
+		'CREATE TABLE products (id, category_id, name, price)',
+		'CREATE INDEX products__category_id ON products (category_id)',
+		'INSERT INTO products (id, category_id, name, price) VALUES (?,?,?,?)',
+		[
+			[1, 5, 'Fancy Hat', '$200'],
+			[2, 5, 'Less Fancy Hat', '$100'],
+			[3, 5, 'Least Fancy Hat', '$5']
+		],
+		'SELECT * FROM products WHERE category_id = ?', [catID]
+	).fail(function (error) {
+		throw new Error(error.message);
+	}).done(function (products) {
+		displayProducts(products);
+	});
+
+Notice how you can insert many rows at once, and how you can return results in the final query.
+
+Lastly, JS is terrible with multiline string. So I've provided a hack that you can use to put multiline SQL into the query
+function. It's a hack using Function.prototype.toString() which returns the contents of a function. It's about the same
+speed as joining an array of strings together to make a multiline string: slow, but effective.
+
+	var db = WebSQL('mydb');
+	db.query(function(){/*!
+		SELECT products.*
+		FROM products
+			INNER JOIN product_categories prod_cats
+				ON prod_cats.id = products.category_id
+		WHERE prod_cats.id = ?
+	*/}, [catID])
+		.fail(function (error) {
+			throw new Error(error.message);
+		}).done(function (products) {
+			displayProducts(products);
+		});
+
+What will happen is the query function will pull the content of the query from the commented section (from the first "/*!"
+to the last "*/". The "!" in the comment is intended to be a marker for minifiers that the comment is to remain intact.
+If this is a problem, you may need to just fall back to standard strings.
+
+Why Support a Deprecated API?
+-----------------------------
+
+Even though the API is deprecated it is the only database on most smart phones. As you can see from CanIUse.com stats,
+Chrome, Safari, and Opera all support this API (http://caniuse.com/sql-storage). This API will be replaced with IndexedDB
+which is a lot friendlier to developers, but is not as well supported (http://caniuse.com/indexeddb) at the moment. The
+web community has come out against using LocalStorage for databases, and there is no clear alternitive for Safari and
+on mobile.
---
+++ src/WebSQL.js
@@ -0,0 +1,276 @@
+(function (context) {
+
+
+
+	// Public object
+	var pub = function WebSQL(name, ver, desc, size, cb) {
+
+		// Open database
+		var
+		db = context.openDatabase && context.openDatabase(name, ver || '1.0', desc || name, size || 5e6, cb),
+
+		// Returned object
+		ret = db && {
+			// Query the database in a transaction
+			query: function (sqls) {
+
+				// Query deferred
+				var df = pub.Deferred(),
+					queries = pub.isArray(sqls) ? sqls : arguments;
+
+				// Create transaction for all queries
+				ret.rawTx(function (tx) {
+					var dfSql = pub.Deferred(),
+						sql, args,
+						i, iLen, j, jLen,
+						succ, error = dfSql.reject;
+
+					// Loop through queries
+					for(i = 0, iLen = queries.length; i < iLen; i++) {
+						sql = queries[i];
+						args = queries[i+1];
+
+						// Convert function into SQL
+						if(typeof sql === 'function') {
+							sql = sql.toString();
+							sql = sql.substr(sql.indexOf('/*!')+3);
+							sql = sql.substr(0, sql.lastIndexOf('*/'));
+						}
+
+						// If query has args
+						if(pub.isArray(args)) {
+							i += 1;
+
+							// If args is actually array of args
+							if(pub.isArray(args[0])) {
+								for(j = 0, jLen = args.length; j < jLen; j++) {
+									if(i + 1 === iLen && j + 1 === jLen) {
+										succ = dfSql.resolve;
+									}
+									tx.executeSql(sql, args[j], succ, error);
+								}
+							}
+
+							// Run query with args
+							else {
+								if(i + 1 === iLen) {
+									succ = dfSql.resolve;
+								}
+								tx.executeSql(sql, args, succ, error);
+							}
+						}
+
+						// Just run the query
+						else {
+							if(i + 1 === iLen) {
+								succ = dfSql.resolve;
+							}
+							tx.executeSql(sql, args, succ, error);
+						}
+					}
+
+					dfSql.fail(df.reject).done(function (tx, res) {
+						var ret = null, i, rows;
+						if(res) {
+							rows = res.rows;
+							if(rows) {
+								ret = [];
+								for(i = 0; i < rows.length; i++) {
+									ret[i] = rows.item(i);
+								}
+							}
+							else if(res.insertId) {
+								ret = res.insertId;
+							}
+						}
+						df.resolve(ret);
+					});
+				});
+
+				// Return a promise for queries
+				return df.promise();
+			},
+
+
+			// Runs a transaction manually on database
+			rawTx: function (fn) {
+				db.transaction(fn);
+			}
+		};
+
+		return ret;
+
+	};
+
+
+	// Public object is public
+	context[pub.name] = pub;
+
+
+
+	// Determine if argument is an array
+	pub.isArray = function (arg) {
+		return !!(arg && arg.constructor === Array);
+	};
+
+
+
+
+	// Deferred results object
+	pub.Deferred = function () {
+		var curState,
+			doneCB = pub.Callbacks(),
+			failCB = pub.Callbacks(),
+			ret = {
+
+				// Return a unique promise object with limited functionality
+				promise: function () {
+					var promise = {};
+					var returnPromise = function (fn) {
+						fn();
+						return promise;
+					};
+
+					promise.always = returnPromise(ret.always);
+					promise.done = returnPromise(ret.done);
+					promise.fail = returnPromise(ret.fail);
+					promise.then = returnPromise(ret.then);
+					promise.with = returnPromise(ret.with);
+					promise.state = ret.state;
+
+					return promise;
+				},
+
+
+				// Returns the state of the deferred
+				state: function () {
+					return curState || 'pending';
+				},
+
+
+				// Complete the deferred successfully
+				resolve: function () {
+					if(!curState) {
+						curState = 'resolved';
+						doneCB.fire.apply(doneCB, arguments);
+					}
+					return ret;
+				},
+
+
+				// Complete the deferred unsuccessfully
+				reject: function () {
+					if(!curState) {
+						curState = 'rejected';
+						failCB.fire.apply(failCB, arguments);
+					}
+					return ret;
+				},
+
+
+				// Add a callback for both states
+				always: function (cb) {
+					return ret.then(cb, cb);
+				},
+
+
+				// Add callbacks for the successful state
+				done: function (cb) {
+					doneCB.add(cb);
+					return ret;
+				},
+
+
+				// Add callbacks for the unsuccessful state
+				fail: function (cb) {
+					failCB.add(cb);
+					return ret;
+				},
+
+
+				// Wrapper for the done/fail functions
+				then: function (succ, err) {
+					return ret.done(succ).fail(err);
+				},
+
+
+				// Complete a deferred with another promise
+				with: function (promise) {
+					promise.done(ret.resolve, ret.reject);
+					return ret;
+				}
+			};
+
+
+		return ret;
+	};
+
+
+	// Take a collection of promises and return a new promise when the collection has finished
+	pub.when = function (promise1) {
+		var i, results = new Array(arguments.length), df = pub.Deferred();
+
+		// Add fail cases, early out
+		for(i = 0; i < arguments.length; i++) {
+			arguments[i].fail(function () {
+				df.reject.apply(this, arguments);
+				results = null;
+			});
+		}
+
+		// Add done cases, collect results
+		for(i = 0; i < arguments.length; i++) {
+			(function (i) {
+				arguments[i].done(function () {
+					results[i] = arguments;
+					for(var j = 0; j < results.length; j++) {
+						if(results[j] === void null) break;
+					}
+					if(j === results.length) {
+						df.resolve.apply(df, results);
+					}
+				});
+			})(i);
+		}
+
+
+		return df.promise();
+	};
+
+
+	// Collection of callbacks which can be triggered once
+	pub.Callbacks = function () {
+		var cbs = [],
+			args,
+			ret = {
+
+				// Has the callbacks been fired
+				hasFired: function () {
+					return !!args;
+				},
+
+
+				// Add a callback to the collection, fire if already fired
+				add: function (cb) {
+					cbs.push(cb);
+					if(ret.hasFired()) {
+						ret.fire.apply(ret, args);
+					}
+				},
+
+
+				// Run the callbacks, passing in arguments
+				fire: function () {
+					args = arguments;
+					for(var i = 0; i < cbs.length; i++) {
+						cbs[i].apply(ret, args);
+					}
+					cbs = [];
+				}
+			};
+		return ret;
+	};
+
+
+})(this);
+
